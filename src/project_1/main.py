5a469fc981b9972aff454bd25df3a27b7464ea4a
caefleury15 <caefleury15@gmail.com>
2024-04-12 14:47:40 +0000

    fix: venv

--- src/project_1/main.py
+++ src/project_1/main.py
@@ -5,4 +5,10 @@ ona.list_sets()
 email_edgelist =  ona.email_edgelist()
 email_vertices = ona.email_vertices()
 
+# URL of the CSV file
+url = 'https://ona-book.org/daya/email_edgelist.csv'
+
+# Read the CSV file from the URL
+df = pd.read_csv(url)
+
 print(email_edgelist.info())
